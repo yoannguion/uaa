e73b963af9de95c85f90959f29319cb390c55e30
Andrew Wittrock <awittrock@pivotal.io>
2019-10-02 15:36:21 -0700

    Test Refactor
    - BackwardsCompatibleTokenEndpointAuthenticationFilterTest
    - Use JUnit 5
    - Use mock* names
    
    [#168740405]
    
    Signed-off-by: Joshua Casey <jcasey@pivotal.io>
    Co-authored-by: Joshua Casey <jcasey@pivotal.io>

--- server/src/test/java/org/cloudfoundry/identity/uaa/authentication/BackwardsCompatibleTokenEndpointAuthenticationFilterTest.java
+++ server/src/test/java/org/cloudfoundry/identity/uaa/authentication/BackwardsCompatibleTokenEndpointAuthenticationFilterTest.java
@@ -3,10 +3,14 @@ package org.cloudfoundry.identity.uaa.authentication;
 import org.cloudfoundry.identity.uaa.oauth.TokenTestSupport;
 import org.cloudfoundry.identity.uaa.provider.oauth.XOAuthAuthenticationManager;
 import org.cloudfoundry.identity.uaa.provider.oauth.XOAuthCodeToken;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.cloudfoundry.identity.uaa.security.PollutionPreventionExtension;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -25,10 +29,10 @@ import static org.cloudfoundry.identity.uaa.oauth.TokenTestSupport.OPENID;
 import static org.cloudfoundry.identity.uaa.oauth.token.ClaimConstants.GRANT_TYPE;
 import static org.cloudfoundry.identity.uaa.oauth.token.TokenConstants.GRANT_TYPE_JWT_BEARER;
 import static org.cloudfoundry.identity.uaa.oauth.token.TokenConstants.GRANT_TYPE_SAML2_BEARER;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.same;
 import static org.mockito.Mockito.mock;
@@ -38,126 +42,141 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 
-public class BackwardsCompatibleTokenEndpointAuthenticationFilterTest {
+@ExtendWith(PollutionPreventionExtension.class)
+class BackwardsCompatibleTokenEndpointAuthenticationFilterTest {
 
-    private AuthenticationManager passwordAuthManager;
-    private SAMLProcessingFilter samlAuthFilter;
-    private XOAuthAuthenticationManager xoAuthAuthenticationManager;
+    private AuthenticationManager mockAuthenticationManager;
+    private SAMLProcessingFilter mockSAMLProcessingFilter;
+    private XOAuthAuthenticationManager mockXOAuthAuthenticationManager;
+    private MockHttpServletRequest mockHttpServletRequest;
+    private MockHttpServletResponse mockHttpServletResponse;
+    private FilterChain mockFilterChain;
+    private AuthenticationEntryPoint mockAuthenticationEntryPoint;
+
     private BackwardsCompatibleTokenEndpointAuthenticationFilter filter;
-    private MockHttpServletRequest request;
-    private MockHttpServletResponse response;
-    private FilterChain chain;
-    private AuthenticationEntryPoint entryPoint;
-    private TokenTestSupport support;
 
-    @Before
-    public void setUp() {
-
-        passwordAuthManager = mock(AuthenticationManager.class);
-        OAuth2RequestFactory requestFactory = mock(OAuth2RequestFactory.class);
-        samlAuthFilter = mock(SAMLProcessingFilter.class);
-        xoAuthAuthenticationManager = mock(XOAuthAuthenticationManager.class);
+    @BeforeEach
+    void setUp() {
+        mockAuthenticationManager = mock(AuthenticationManager.class);
+        OAuth2RequestFactory mockOAuth2RequestFactory = mock(OAuth2RequestFactory.class);
+        mockSAMLProcessingFilter = mock(SAMLProcessingFilter.class);
+        mockXOAuthAuthenticationManager = mock(XOAuthAuthenticationManager.class);
 
         filter = spy(
                 new BackwardsCompatibleTokenEndpointAuthenticationFilter(
-                        passwordAuthManager,
-                        requestFactory,
-                        samlAuthFilter,
-                        xoAuthAuthenticationManager
+                        mockAuthenticationManager,
+                        mockOAuth2RequestFactory,
+                        mockSAMLProcessingFilter,
+                        mockXOAuthAuthenticationManager
                 )
         );
 
-        entryPoint = mock(AuthenticationEntryPoint.class);
-        filter.setAuthenticationEntryPoint(entryPoint);
+        mockAuthenticationEntryPoint = mock(AuthenticationEntryPoint.class);
+        filter.setAuthenticationEntryPoint(mockAuthenticationEntryPoint);
 
-        request = new MockHttpServletRequest("POST", "/oauth/token");
-        response = new MockHttpServletResponse();
-        chain = mock(FilterChain.class);
+        mockHttpServletRequest = new MockHttpServletRequest("POST", "/oauth/token");
+        mockHttpServletResponse = new MockHttpServletResponse();
+        mockFilterChain = mock(FilterChain.class);
     }
 
-    @After
-    public void tearDown() {
+    @AfterEach
+    void tearDown() {
         SecurityContextHolder.clearContext();
-        ofNullable(support).ifPresent(TokenTestSupport::clear);
     }
 
     @Test
-    public void password_expired() throws Exception {
+    void password_expired() throws Exception {
         UaaAuthentication uaaAuthentication = mock(UaaAuthentication.class);
         when(uaaAuthentication.isAuthenticated()).thenReturn(true);
         when(uaaAuthentication.isRequiresPasswordChange()).thenReturn(true);
-        when(passwordAuthManager.authenticate(any())).thenReturn(uaaAuthentication);
-        request.addParameter(GRANT_TYPE, "password");
-        request.addParameter("username", "marissa");
-        request.addParameter("password", "koala");
-        filter.doFilter(request, response, chain);
-        verify(entryPoint, times(1)).commence(same(request), same(response), any(PasswordChangeRequiredException.class));
+        when(mockAuthenticationManager.authenticate(any())).thenReturn(uaaAuthentication);
+        mockHttpServletRequest.addParameter(GRANT_TYPE, "password");
+        mockHttpServletRequest.addParameter("username", "marissa");
+        mockHttpServletRequest.addParameter("password", "koala");
+        filter.doFilter(mockHttpServletRequest, mockHttpServletResponse, mockFilterChain);
+        verify(mockAuthenticationEntryPoint, times(1)).commence(same(mockHttpServletRequest), same(mockHttpServletResponse), any(PasswordChangeRequiredException.class));
     }
 
     @Test
-    public void attempt_password_authentication() throws Exception {
-        request.addParameter(GRANT_TYPE, "password");
-        request.addParameter("username", "marissa");
-        request.addParameter("password", "koala");
-        filter.doFilter(request, response, chain);
-        verify(filter, times(1)).attemptTokenAuthentication(same(request), same(response));
-        verify(passwordAuthManager, times(1)).authenticate(any());
-        verifyZeroInteractions(samlAuthFilter);
-        verifyZeroInteractions(xoAuthAuthenticationManager);
+    void attempt_password_authentication() throws Exception {
+        mockHttpServletRequest.addParameter(GRANT_TYPE, "password");
+        mockHttpServletRequest.addParameter("username", "marissa");
+        mockHttpServletRequest.addParameter("password", "koala");
+        filter.doFilter(mockHttpServletRequest, mockHttpServletResponse, mockFilterChain);
+        verify(filter, times(1)).attemptTokenAuthentication(same(mockHttpServletRequest), same(mockHttpServletResponse));
+        verify(mockAuthenticationManager, times(1)).authenticate(any());
+        verifyZeroInteractions(mockSAMLProcessingFilter);
+        verifyZeroInteractions(mockXOAuthAuthenticationManager);
     }
 
     @Test
-    public void attempt_saml_assertion_authentication() throws Exception {
-        request.addParameter(GRANT_TYPE, GRANT_TYPE_SAML2_BEARER);
-        request.addParameter("assertion", "saml-assertion-value-here");
-        filter.doFilter(request, response, chain);
-        verify(filter, times(1)).attemptTokenAuthentication(same(request), same(response));
-        verify(samlAuthFilter, times(1)).attemptAuthentication(same(request), same(response));
-        verifyZeroInteractions(passwordAuthManager);
-        verifyZeroInteractions(xoAuthAuthenticationManager);
+    void attempt_saml_assertion_authentication() throws Exception {
+        mockHttpServletRequest.addParameter(GRANT_TYPE, GRANT_TYPE_SAML2_BEARER);
+        mockHttpServletRequest.addParameter("assertion", "saml-assertion-value-here");
+        filter.doFilter(mockHttpServletRequest, mockHttpServletResponse, mockFilterChain);
+        verify(filter, times(1)).attemptTokenAuthentication(same(mockHttpServletRequest), same(mockHttpServletResponse));
+        verify(mockSAMLProcessingFilter, times(1)).attemptAuthentication(same(mockHttpServletRequest), same(mockHttpServletResponse));
+        verifyZeroInteractions(mockAuthenticationManager);
+        verifyZeroInteractions(mockXOAuthAuthenticationManager);
     }
 
     @Test
-    public void saml_assertion_missing() throws Exception {
-        request.addParameter(GRANT_TYPE, GRANT_TYPE_SAML2_BEARER);
-        filter.doFilter(request, response, chain);
-        verify(filter, times(1)).attemptTokenAuthentication(same(request), same(response));
-        verifyZeroInteractions(xoAuthAuthenticationManager);
-        verifyZeroInteractions(passwordAuthManager);
-        verifyZeroInteractions(xoAuthAuthenticationManager);
+    void saml_assertion_missing() throws Exception {
+        mockHttpServletRequest.addParameter(GRANT_TYPE, GRANT_TYPE_SAML2_BEARER);
+        filter.doFilter(mockHttpServletRequest, mockHttpServletResponse, mockFilterChain);
+        verify(filter, times(1)).attemptTokenAuthentication(same(mockHttpServletRequest), same(mockHttpServletResponse));
+        verifyZeroInteractions(mockXOAuthAuthenticationManager);
+        verifyZeroInteractions(mockAuthenticationManager);
+        verifyZeroInteractions(mockXOAuthAuthenticationManager);
         ArgumentCaptor<AuthenticationException> exceptionArgumentCaptor = ArgumentCaptor.forClass(AuthenticationException.class);
-        verify(entryPoint, times(1)).commence(same(request), same(response), exceptionArgumentCaptor.capture());
+        verify(mockAuthenticationEntryPoint, times(1)).commence(same(mockHttpServletRequest), same(mockHttpServletResponse), exceptionArgumentCaptor.capture());
         assertNotNull(exceptionArgumentCaptor.getValue());
         assertEquals("SAML Assertion is missing", exceptionArgumentCaptor.getValue().getMessage());
         assertTrue(exceptionArgumentCaptor.getValue() instanceof InsufficientAuthenticationException);
     }
 
-    @Test
-    public void attempt_jwt_token_authentication() throws Exception {
-        support = new TokenTestSupport(null);
-        String idToken = support.getIdTokenAsString(Collections.singletonList(OPENID));
-        request.addParameter(GRANT_TYPE, GRANT_TYPE_JWT_BEARER);
-        request.addParameter("assertion", idToken);
-        filter.doFilter(request, response, chain);
-        verify(filter, times(1)).attemptTokenAuthentication(same(request), same(response));
-        ArgumentCaptor<XOAuthCodeToken> authenticateData = ArgumentCaptor.forClass(XOAuthCodeToken.class);
-        verify(xoAuthAuthenticationManager, times(1)).authenticate(authenticateData.capture());
-        verifyZeroInteractions(passwordAuthManager);
-        verifyZeroInteractions(xoAuthAuthenticationManager);
-        assertEquals(idToken, authenticateData.getValue().getIdToken());
-        assertNull(authenticateData.getValue().getOrigin());
+    @Nested
+    @ExtendWith(PollutionPreventionExtension.class)
+    class WithSupport {
+
+        private TokenTestSupport support;
+
+        @BeforeEach
+        void setUp() throws Exception {
+            support = new TokenTestSupport(null);
+        }
+
+        @AfterEach
+        void tearDown() {
+            support.clear();
+        }
+
+        @Test
+        void attempt_jwt_token_authentication() throws Exception {
+            String idToken = support.getIdTokenAsString(Collections.singletonList(OPENID));
+            mockHttpServletRequest.addParameter(GRANT_TYPE, GRANT_TYPE_JWT_BEARER);
+            mockHttpServletRequest.addParameter("assertion", idToken);
+            filter.doFilter(mockHttpServletRequest, mockHttpServletResponse, mockFilterChain);
+            verify(filter, times(1)).attemptTokenAuthentication(same(mockHttpServletRequest), same(mockHttpServletResponse));
+            ArgumentCaptor<XOAuthCodeToken> authenticateData = ArgumentCaptor.forClass(XOAuthCodeToken.class);
+            verify(mockXOAuthAuthenticationManager, times(1)).authenticate(authenticateData.capture());
+            verifyZeroInteractions(mockAuthenticationManager);
+            verifyZeroInteractions(mockXOAuthAuthenticationManager);
+            assertEquals(idToken, authenticateData.getValue().getIdToken());
+            assertNull(authenticateData.getValue().getOrigin());
+        }
     }
 
     @Test
-    public void jwt_assertion_missing() throws Exception {
-        request.addParameter(GRANT_TYPE, GRANT_TYPE_JWT_BEARER);
-        filter.doFilter(request, response, chain);
-        verify(filter, times(1)).attemptTokenAuthentication(same(request), same(response));
-        verifyZeroInteractions(xoAuthAuthenticationManager);
-        verifyZeroInteractions(passwordAuthManager);
-        verifyZeroInteractions(xoAuthAuthenticationManager);
+    void jwt_assertion_missing() throws Exception {
+        mockHttpServletRequest.addParameter(GRANT_TYPE, GRANT_TYPE_JWT_BEARER);
+        filter.doFilter(mockHttpServletRequest, mockHttpServletResponse, mockFilterChain);
+        verify(filter, times(1)).attemptTokenAuthentication(same(mockHttpServletRequest), same(mockHttpServletResponse));
+        verifyZeroInteractions(mockXOAuthAuthenticationManager);
+        verifyZeroInteractions(mockAuthenticationManager);
+        verifyZeroInteractions(mockXOAuthAuthenticationManager);
         ArgumentCaptor<AuthenticationException> exceptionArgumentCaptor = ArgumentCaptor.forClass(AuthenticationException.class);
-        verify(entryPoint, times(1)).commence(same(request), same(response), exceptionArgumentCaptor.capture());
+        verify(mockAuthenticationEntryPoint, times(1)).commence(same(mockHttpServletRequest), same(mockHttpServletResponse), exceptionArgumentCaptor.capture());
         assertNotNull(exceptionArgumentCaptor.getValue());
         assertEquals("Assertion is missing", exceptionArgumentCaptor.getValue().getMessage());
         assertTrue(exceptionArgumentCaptor.getValue() instanceof InsufficientAuthenticationException);
